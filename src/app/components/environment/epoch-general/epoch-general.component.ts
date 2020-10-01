6601db6353b946f8324656e4c83e5c79cc110fe7
Anthony Trombetta <atrombet@gmail.com>
2020-09-30 20:20:11 -0500

    FAN-13 Enable/Disable Epoch Time
    Add toggle switch to enable or disable epoch time.
    
    Changes:
    Add toggle switch to epoch general component.
    Update epoch general form factory.
    Update epoch general component.

--- src/app/components/environment/epoch-general/epoch-general.component.ts
+++ src/app/components/environment/epoch-general/epoch-general.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Environment } from '../../../interfaces';
+import { EnvironmentService } from '../../../services';
 
 @Component({
   selector: 'epoch-general',
   templateUrl: './epoch-general.component.html',
   styleUrls: ['./epoch-general.component.scss']
 })
-export class EpochGeneralComponent implements OnInit {
+export class EpochGeneralComponent implements OnInit, OnDestroy {
+  private subs: Subscription = new Subscription();
 
-  constructor() { }
+  public env: Environment;
+  public form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private envService: EnvironmentService) {}
+
+  /**
+   * Angular life cycle hook On Init
+   */
+  public ngOnInit(): void {
+    this.subs.add(
+      this.route.params.subscribe(({ id }) => {
+        this.env = this.envService.getEnvById(parseInt(id, 10));
+        if (this.env) {
+          this.form = this.env.sections
+            .find(section => section.name === 'epoch')
+            .subsections
+            .find(subsection => subsection.name === 'general')
+            .form;
+        }
+      })
+    );
+  }
+
+  /**
+   * Angular life cycle hook On Destroy
+   */
+  public ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
 }
